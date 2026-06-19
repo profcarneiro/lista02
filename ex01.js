92e9551fa17f7b3988ada2001f6819dcf3273f22
profcarneiro <jorge.carneiro@sp.senai.br>
2026-06-19 10:53:30 -0300

    Exercício 01 finalizado

--- ex01.js
+++ ex01.js
@@ -1,1 +1,16 @@
-console.log("Olá mundo");
+//Sortear 20 numeros entre 0 e 10
+count = 0;
+divs = 0;
+for (i = 1; i < 21; i++) {
+    sorteado = Math.floor(Math.random() * 10);
+    console.log(`Sorteio numero: ${i}`);
+    console.log(`Sorteado: ${sorteado}`);
+    if (sorteado > 5) {
+        count = count + 1;
+    }
+    if (sorteado % 3 == 0) {
+        divs = divs + 1;
+    }
+}
+console.log(`Numeros maiores que 5: ${count}`);
+console.log(`Numeros divisiveis por 3: ${divs}`);
